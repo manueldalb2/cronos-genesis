8b3f97b09f8f5ac801552c00e3c972afbb6b0932
manueldalb2 <ghost.mdb@gmail.com>
2026-02-25 11:24:02 +0000

    v1.5.4g: fix upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -13,7 +13,7 @@ import (
 
 // RegisterUpgradeHandlers returns if store loader is overridden
 func (app *App) RegisterUpgradeHandlers(cdc codec.BinaryCodec, maxVersion int64) bool {
-	planName := "v1.5" // TBD
+	planName := "v1.5.0" // TBD
 	app.UpgradeKeeper.SetUpgradeHandler(planName, func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		return app.ModuleManager.RunMigrations(ctx, app.configurator, fromVM)
 	})
